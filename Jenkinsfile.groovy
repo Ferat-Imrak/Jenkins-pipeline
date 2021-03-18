a6febf3e3cdff2ccffced0f9153adab107b198f6
jiro1 <feratimrak1@gmail.com>
2021-03-18 18:33:07 -0500

    change directry

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,19 +11,21 @@ properties([
 
 node {
     stage("Terraform Pipeline"){
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/jiro1/vpc-jenkins-job.git']]])
+        checkout([
+            $class: 'GitSCM', branches: [[name: '*/master']], extensions: [], 
+            userRemoteConfigs: [[url: 'https://github.com/jiro1/vpc-jenkins-job.git']]])
     }
 
-//     stage("Format"){
-// 		timestamps {
-//             ws("workspace/vpc-jenkins-job/vpc"){
-//                 sh "make f"
-//             }
-//     }
-// }
+    stage("Format"){
+		timestamps {
+            ws("workspace/vpc-jenkins-job"){
+                sh "make f"
+            }
+    }
+}
 	stage("Initialize"){
 		timestamps {
-            ws("workspace/vpc-jenkins-job/vpc"){
+            ws("workspace/vpc-jenkins-job"){
                 sh "make i"
             }
     }
