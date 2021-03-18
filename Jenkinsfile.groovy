42c0872f842313fe0bcea59f8c914ea0bbeed500
jiro1 <feratimrak1@gmail.com>
2021-03-18 16:29:55 -0500

    added email notification

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,4 +15,8 @@ node {
     stage("Stage4"){
         echo "Hello"
     }
+
+    stage("Send Email to Support"){
+		mail bcc: '', body: 'Running', cc: 'support@company.com', from: '', replyTo: '', subject: 'Test', to: 'farrukhsadykov@gmail.com'
+	}
 }
